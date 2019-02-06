f69d364b3d08b98412b517274f2c1992a3834b9c
Uinotin <uinotin@gmail.com>
2019-02-06 10:55:34 +0200

    Logger / Writer / Example: First working version
    Separated functionality into different classes and separated the programme into two threads: the main thread that sends the log messages and another that prints them forward.

--- logger/Logger.cpp
+++ logger/Logger.cpp
@@ -4,27 +4,90 @@
 #include <sstream>
 #include <thread>
 
+Writer::Writer()
+{
+}
+
+Writer::~Writer() = default;
+
+Writer::Writer(const Writer&)
+{
+}
+
 Logger * Logger::m_instance = nullptr;
 
 class Logger::Impl
 {
 public:
   Impl()
-  {}
-private:
+    : m_numTasks(0)
+  {
+    /// Add enough writers to prevent the programme from getting stuck,
+    /// if one thread takes longer to unlock the writer.
+    m_writers.resize(32);
+    m_currentWriter = m_writers.begin();
+    m_currentWriter->m_mutex.lock();
+
+    m_printThread = std::thread([this] { printThread(); });
+  }
+  void printThread();
+
+  std::vector<Writer> m_writers;
+  std::vector<Writer>::iterator m_currentWriter;
+
+  std::thread m_printThread;
+  int m_numTasks;
+  bool m_notClosing;
+
+  std::mutex m_writerGetterMutex;
 };
+
+void Logger::Impl::printThread()
+{
+  auto currentWriter = m_writers.begin();
+  currentWriter->m_mutex.lock();
+  while (m_notClosing || m_numTasks) {
+    std::cout << currentWriter->m_stream.str();
+    currentWriter->m_stream.str(std::string());
+    --m_numTasks;
+    auto nextWriter = currentWriter + 1;
+    if (nextWriter == m_writers.end())
+      nextWriter = m_writers.begin();
+    nextWriter->m_mutex.lock();
+    currentWriter->m_mutex.unlock();
+    currentWriter = nextWriter;
+  }
+}
 
 Logger * Logger::Instance()
 {
   if (!m_instance)
     m_instance = new Logger;
+
   return m_instance;
 }
 
+WriterExclusivePtr Logger::getWriter()
+{
+  std::lock_guard<std::mutex> lockGuard(m_impl->m_writerGetterMutex);
+  auto returnWriter = m_impl->m_currentWriter++;
+  if (m_impl->m_currentWriter == m_impl->m_writers.end())
+    m_impl->m_currentWriter = m_impl->m_writers.begin();
+  m_impl->m_currentWriter->m_mutex.lock();
+
+  ++m_impl->m_numTasks;
+  return WriterExclusivePtr(&(*returnWriter));
+}
+void Logger::stop()
+{
+  m_impl->m_writerGetterMutex.lock();
+  m_impl->m_notClosing = false;
+  m_impl->m_currentWriter->m_mutex.unlock();
+  m_impl->m_printThread.join();
+}
 Logger::Logger()
   : m_impl(new Impl)
 {
-  std::cout << "Initialised logger" << std::endl;
 }
 
 Logger::~Logger() = default;
--- logger/Logger.hpp
+++ logger/Logger.hpp
@@ -1,32 +1,96 @@
 #ifndef LOGGER_HPP
 #define LOGGER_HPP
+
 #include <memory>
 #include <iostream>
 #include <sstream>
+#include <mutex>
+#include <vector>
+
+class Writer
+{
+  friend class WriterExclusivePtr;
+  friend class Logger;
+public:
+  Writer();
+  ~Writer();
+  Writer(const Writer&);
+
+  Writer & operator << (std::ostream& data(std::ostream&))
+  {
+    m_stream << data;
+    return *this;
+  }
+  template<typename T> Writer & operator << (const T& data)
+  {
+    m_stream << data;
+    return *this;
+  }
+
+  std::mutex m_mutex;
+private:
+  std::stringstream m_stream;
+};
+
+/// Class around Writer that unlocks Writer's mutex once WriterExclusivePtr is deleted
+class WriterExclusivePtr
+{
+  friend class Logger;
+public:
+  WriterExclusivePtr(WriterExclusivePtr && ptr)
+    : m_ptr(ptr.m_ptr)
+  {
+    ptr.m_ptr = nullptr;
+  }
+  ~WriterExclusivePtr()
+  {
+    m_ptr->m_mutex.unlock();
+  }
+  Writer * operator ->()
+  {
+    return m_ptr;
+  }
+  Writer const * operator ->() const
+  {
+    return m_ptr;
+  }
+  Writer & operator << (std::ostream& data(std::ostream&))
+  {
+    return *m_ptr << data;
+  }
+  template<typename T> Writer & operator << (const T& data)
+  {
+    return (*m_ptr << data);
+  }
+
+private:
+  WriterExclusivePtr(Writer *ptr)
+    : m_ptr(ptr)
+  {
+  }
+  WriterExclusivePtr(WriterExclusivePtr const &)
+  {
+  }
+  WriterExclusivePtr & operator = (WriterExclusivePtr const &)
+  {
+    return *this;
+  }
+  Writer * m_ptr;
+};
 
 class Logger
 {
 public:
   static Logger * Instance();
-  Logger & operator << (std::ostream& data(std::ostream&))
-  {
-    m_stream << data;
-    return *this;
-  }
-  template<typename T> Logger & operator << (const T& data)
-  {
-    m_stream << data;
-    return *this;
-  }
+  WriterExclusivePtr getWriter();
+  void stop();
 
 private:
   Logger();
   ~Logger();
-
+  static Logger * m_instance;
   class Impl;
   std::unique_ptr<Impl> m_impl;
-  std::stringstream m_stream;
-  static Logger * m_instance;
 };
 
 class NullLogger
@@ -43,15 +107,16 @@ public:
 };
 static NullLogger s_nullLogger;
 
-#define VERBOSE (*(Logger::Instance()) << "Verbose: ")
-#define INFO (*(Logger::Instance()) << "Info: ")
+#define VERBOSE (Logger::Instance()->getWriter() << "Verbose: ")
+#define INFO (Logger::Instance()->getWriter() << "Info: ")
 #ifdef _DEBUG
-#define DEBUG (*(Logger::Instance()) << "Debug: ")
+#define DEBUG (Logger::Instance()->getWriter() << "Debug: ")
 #else
 #define DEBUG s_nullLogger
 #endif
-#define ERROR (*(Logger::Instance()) << "Error: ")
-#define FATAL (*(Logger::Instance()) << "FATAL: ")
+#define ERROR (Logger::Instance()->getWriter() << "Error: ")
+#define FATAL (Logger::Instance()->getWriter() << "FATAL: ")
+#define STOPLOGGER Logger::Instance()->stop();
 
 
 #endif /// LOGGER_HPP
